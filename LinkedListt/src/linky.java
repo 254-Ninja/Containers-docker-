138c25403bea5e6dd83bd900611495f1dc0111c4
254-ninja <muthomidenis62@gmail.com>
2021-01-19 02:56:41 +0300

    Linked List

--- LinkedListt/src/linky.java
+++ LinkedListt/src/linky.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
 
 public class linky {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList linky = new LinkedList();
+		
+		linky.add("Rob");
+		linky.add("Alex");
 
 	}
 
