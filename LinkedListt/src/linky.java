8e5ead104d5665c78dd4110acf2b98b8b13a238e
254-ninja <muthomidenis62@gmail.com>
2021-01-19 15:33:44 +0300

    Linked List

--- LinkedListt/src/linky.java
+++ LinkedListt/src/linky.java
@@ -3,11 +3,16 @@ import java.util.LinkedList;
 public class linky {
 
 	public static void main(String[] args) {
-		LinkedList linky = new LinkedList();
+		
+		LinkedList<String> linky = new LinkedList<String>();
 		
 		linky.add("Rob");
 		linky.add("Alex");
 		linky.add("Dennis");
+		
+		linky.getFirst();
+		
+		
 		
 		System.out.println(linky);
 
